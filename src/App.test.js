fbd5a0b1ec03e99f9d51a3abd6635708d16c4a35
Nicolas Thouvenin <nthouvenin@gmail.com>
2026-01-21 13:34:22 +0100

    reset change

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
-import { render } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  render(<App />);
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
